35e7b218b009ccb3fef4b37ee1e2c09ca5abba81
mikerah <mikerah@gmail.com>
2020-09-17 10:22:44 -0500

    code from module 1

---
+++ module1.py
@@ -0,0 +1,39 @@
+# Module 1
+# Mike Goldman
+
+
+# int
+num1 = 1
+
+
+print(num1)
+
+# float
+num2 = 1.1
+
+print(num2)
+
+# boolean
+
+status = True
+
+print(status)
+
+# string
+mystr = "Hello class"
+
+print(mystr)
+
+
+# tuple
+
+mytup = (1, 2, 3, 4)
+
+print(mytup)
+
+
+# dictionary
+
+mydict = {"key1": 1, "key2": 2, "key3": 3}
+
+print(mydict)
